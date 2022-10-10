13c70d09a7299316e79c3d6860445cef5c2271bc
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-10 14:46:24 +0200

    classe astratta Prodotto; aggiornato gitignore

---
+++ src/com/soprasteria/javainheritanceshop/prodotto/Prodotto.java
@@ -0,0 +1,39 @@
+package com.soprasteria.javainheritanceshop.prodotto;
+
+public abstract class Prodotto {
+	
+	private int codice;
+	private String nome;
+	private String marca;
+	private double prezzo;
+	private double iva;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public double getPrezzo() {
+		return prezzo;
+	}
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+	public double getIva() {
+		return iva;
+	}
+	public void setIva(double iva) {
+		this.iva = iva;
+	}
+	public int getCodice() {
+		return codice;
+	}
+
+}
